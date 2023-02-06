e237eb482f0a5cb3b2da723d575d2b44bd8cbf76
MarynaAponasko <aponasko.mari@gmail.com>
2023-02-06 17:35:35 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import Statistics from './Statistics/Statistics';
 import Section from './Section/Section';
 import Notification from './Notification/Notification';
 
-const App = ({}) => {
+const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
@@ -27,7 +27,6 @@ const App = ({}) => {
       default:
         return;
     }
-    // console.log(name);
   };
 
   const countPositiveFeedbackPercentage = () => {
